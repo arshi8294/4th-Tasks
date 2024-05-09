ddcbac629adc72f5bc085e97604df5ab7dda6764
Arshia <arshi829495@gmail.com>
2024-05-10 01:27:23 +0330

    created-&-migrated-&-registered-person-model

--- MyCV/cv/admin.py
+++ MyCV/cv/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Profile  # import model
 
-# Register your models here.
+admin.site.register(Profile)  # add model to admin panel
--- MyCV/cv/models.py
+++ MyCV/cv/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=11)
+    summary = models.TextField()
+    skills = models.TextField()
+    experience = models.TextField()
+    education = models.TextField()
